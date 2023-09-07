27ce09c77aafff3fe5811032a86ccba54d95a803
Emeson12345678910 <124007178+Emeson12345678910@users.noreply.github.com>
2023-09-06 21:27:11 -0600

    Add files via upload

---
+++ GUIA_3_SEMANA_6_EJERCICIO_2/GUIA_3_SEMANA_6_EJERCICIO_2/Program.cs
@@ -0,0 +1,22 @@
+﻿class Operacionn
+{
+    static void Main()
+    {
+        int[] scores = { 90, 71, 82, 93, 75, 82 };
+
+        var queryLowNums =
+            from num2 in scores
+            where num2 > 80
+            orderby num2 descending
+            select num2;
+
+        foreach (var num2 in queryLowNums)
+        {
+            Console.WriteLine(num2);
+            {
+                static bool isEven(int i)
+                { return i % 2 == 0; }
+            }
+        }
+    }
+}
---
+++ Guia_3_semana_6/Guia_3_semana_6/Program.cs
@@ -0,0 +1,23 @@
+﻿
+//EJERCICIO 1
+class Operacion
+{
+    static void Main()
+    {
+        int[] scores = { 97, 92, 81, 60, };
+
+        var queryLowNums =
+            from num in scores
+            where num > 80
+            orderby num descending
+            select num;
+        foreach (var num in queryLowNums)
+        {
+            Console.WriteLine("NUmeros mayor a 80");
+            Console.WriteLine(num);
+
+        }
+
+    }
+}
+
